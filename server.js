91a5dec7d473cbe955be183e64bb111a02f43476
Roshan Singh <roshan@localhost.localdomain>
2019-12-26 13:03:35 +0530

    mongoose connection established and colorjs added

--- server.js
+++ server.js
@@ -2,15 +2,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-//Route files
-const bootcamps = require("./routes/bootcamps");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 //custom Logger middleware
 // const logger = require("./middleware/logger");
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+//Route files
+const bootcamps = require("./routes/bootcamps");
+
+//Initalize mongo
+connectDB();
 
 //Initialize express
 const app = express();
@@ -30,7 +35,16 @@ app.use("/api/v1/bootcamps", bootcamps);
 const PORT = process.env.PORT || 5000;
 
 //express serve
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server is runnning in ${process.env.NODE_ENV} mode on ${PORT}`)
+  console.log(
+    `Server is runnning in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
+  )
 );
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  //Close Server & exit process
+  server.close(() => process.exit(1));
+});
